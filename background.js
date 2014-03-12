a6102d683da76b0449c7e97f836d0f95bc9976d4
Daniel X Moore <yahivin@gmail.com>
2014-03-12 12:54:58 -0700

    :left_right_arrow::bamboo: Updated in browser at strd6.github.io/editor

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
     'bounds': {
-      'width': undefined,
-      'height': undefined
+      'width': 480,
+      'height': 320
     }
   });
 });
